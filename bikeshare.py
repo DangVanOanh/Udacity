3bbee86a27ea484e7a79b3f7b176af2147189c4c
Oanh Dang <oanhdvcntt@hotmail.com>
2024-09-30 23:55:00 +0700

    update  bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -6,13 +6,99 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-def load_data(city):
-    data_city = pd.read_csv(city)
-    df = pd.DataFrame(data_city)
+Months = {
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'All'
+}
+
+
+days_of_week = {
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'All'
+}
+
+# Function to filter DataFrame by month name input
+def filter_by_month_name(df, month_name, columnDate):
+    # Extract month names from the datetime column
+    month_name = month_name.capitalize()  # Ensure the input is correctly capitalized
+    filtered_df = df[df[columnDate].dt.strftime('%B') == month_name]
+    return filtered_df
+
+# Function to filter DataFrame by day name input
+def filter_by_day_name(df, day_name, columnDate):
+    # Extract day names from the datetime column
+    day_name = day_name.capitalize()  # Ensure the input is correctly capitalized
+    filtered_df = df[df[columnDate].dt.strftime('%A') == day_name]
+    return filtered_df
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    file_data_city = pd.read_csv(CITY_DATA[city])
+    df = pd.DataFrame(file_data_city)
+    if month != 'All':
+       df['Start Time'] = pd.to_datetime(df['Start Time'])
+       df = filter_by_month_name(df, month, 'Start Time')
+
+    if day != 'All':
+       df['Start Time'] = pd.to_datetime(df['Start Time'])
+       df = filter_by_day_name(df, day,'Start Time')
     return df
+
+# Function to validate City input
+def validate_City(city):
+    if city in CITY_DATA:
+        return True
+    else:
+        return False
+
+# Function to validate month input
+def validate_month(month):
+    if month in Months:
+        return True
+    else:
+        return False
+
+# Function to validate day input
+def validate_day_of_week(day):
+    if day in days_of_week:
+        return True
+    else:
+        return False
+
+def get_filters():
+    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    while True:
+        city = input("Please enter City name:")
+        if validate_City(city.lower()) == True:
+            break
+
+    # get user input for month (all, january, february, ... , june)
+    while True:
+        month = input("Please enter month name:")
+        if validate_month(month) == True:
+            break
+
+    # get user input for day of week (all, monday, tuesday, ... sunday)
+    while True:
+        day = input("Please enter day name:")
+        if validate_day_of_week(day) == True:
+            break
+    
+    return city, month, day
 
 def time_stats(df):
 
+    if df.empty:
+        return False
+    
     data_convert = pd.to_datetime(df['Start Time'])
 
     # most common month
@@ -24,19 +110,26 @@ def time_stats(df):
     # most common hour of day
     hourOfday = data_convert.dt.hour.value_counts().idxmax()
     
-    return month, dayOfweek, hourOfday
+    print(f"Most month = {month}:==> most day of week = {dayOfweek}:==> most hour of day = {hourOfday}")
 
 def station_stats(df):
+
+    if df.empty:
+        return False
+    
     start_station = df['Start Station'].value_counts().idxmax()
     end_station = df['End Station'].value_counts().idxmax()
 
-    df['trip'] = df['Start Station'] + ' to ' + df['End Station']
+    df['trip'] = df['Start Station'] + ' TO ' + df['End Station']
 
     trip_counts = df['trip'].value_counts().idxmax()
     
-    return start_station, end_station, trip_counts
+    print(f"Most start station = {start_station}:==> most end station = {end_station}:==> most trip = {trip_counts} ")
 
 def trip_duration_stats(df):
+
+    if df.empty:
+        return False
      
     # Convert 'start_time' and 'end_time' to datetime
     start_time = pd.to_datetime(df['Start Time'])
@@ -45,56 +138,66 @@ def trip_duration_stats(df):
     # Calculate the duration for each trip
     df['duration'] = end_time - start_time
 
+    #caculation total travel time
     total_duration = df.groupby(['Start Time','End Time'])['duration'].sum()
 
+    #caculation mean travel time
     avg_duration = df.groupby(['Start Time','End Time'])['duration'].mean()
 
-    return total_duration, avg_duration
+    print(f"total_duration = {total_duration}:==> avg_duration = {avg_duration}" )
 
-def user_stats(series, birthday = None):
+def count_by_series(series):
 
-    if birthday != None:
-        birth_day = series
-        birth_day_earliest = birth_day.min()
-        birth_day_recent = birth_day.max()
-        most_year = birth_day.value_counts().idxmax()
-        return birth_day_earliest, birth_day_recent, most_year
-    
+    if series.empty:
+        return False
     count_user_types = {}
     for user in series:
         if user in count_user_types:
             count_user_types[user] += 1
         else:
             count_user_types[user] = 1
-
+    
     return count_user_types
+def user_stats(df, city = None):
+
+    if df.empty:
+        return False
+    #counts of each user type
+    counts_user_type = count_by_series(df['User Type'])
+
+    if city != 'washington':
+        #counts of each gender (only available for NYC and Chicago)
+        counts_user_gender = count_by_series(df['Gender'])
+
+        #earliest, most recent, most common year of birth (only available for NYC and Chicago)
+        birth_day = df['Birth Year']
+        birth_day_earliest = birth_day.min()
+        birth_day_recent = birth_day.max()
+        most_year = birth_day.value_counts().idxmax()
+
+        print(f"counts of each gender = {counts_user_gender}")
+        print(f"birth_day_earliest = {birth_day_earliest}: birth_day_recent = {birth_day_recent}: most_year = {most_year} ")
+
+    print(f"counts of each user type = {counts_user_type})")
 
 def main():
-    for key, city in CITY_DATA.items():
-        df = load_data(city)
+    
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
 
-        #1 Popular times of travel
-        month, dayOfweek, hourOfday = time_stats(df)
-        print(f"CITY = {key}: ==== Most month = {month}: most day of week = {dayOfweek}: most hour of day = {hourOfday}")
+        if df.empty:
+            print('No data matches the input condition')
+        else:
+            print(df)
+            time_stats(df)
+            station_stats(df)
+            trip_duration_stats(df)
+            user_stats(df, city)
 
-        #2 Popular stations and trip
-        start_station, end_station, trip_counts = station_stats(df)
-        print(f"CITY = {key}: ==== Most start station = {start_station}: most end station = {end_station}: most trip = {trip_counts} ")
-
-        #3 Trip duration
-        total_duration, avg_duration = trip_duration_stats(df)
-        print(f"CITY = {key}: ==== total_duration = {total_duration}: avg_duration = {avg_duration}" )
-
-        #4 User info
-        count_user_types = user_stats(df['User Type'])
-        print(count_user_types)
-
-        if key != "washington":
-            gender_count = user_stats(df['Gender'])
-            print(gender_count)
-
-            birth_day_earliest, birth_day_recent, most_year = user_stats(df['Birth Year'], birthday=key)
-            print(f"CITY = {key}: birth_day_earliest = {birth_day_earliest}: birth_day_recent = {birth_day_recent}: most_year = {most_year} ")
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
 
 if __name__ == "__main__":
 	main()
